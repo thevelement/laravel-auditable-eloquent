33406cc1a930c1d79b1a0d098510bd056ec34263
vinny <vinny@host.thevelement.com>
2015-10-01 23:51:48 -0400

    Centralized Packagist Directory

---
+++ src/Thevelement/LaravelAuditableEloquent/LaravelAuditableMySqlSchemaBuilder.php
@@ -0,0 +1,33 @@
+<?php 
+
+namespace Thevelement\LaravelAuditableEloquent;
+
+use Illuminate\Database\Schema\MySqlBuilder;
+
+class LaravelAuditableMySqlSchemaBuilder extends MySqlBuilder {
+	public function timestamps($auditField = [], $auditIdType = 'unsignedInteger')
+	{
+		$this->timestamp('created_at');
+		
+		if (in_array('created_at', $auditField)) $this->audit(['created_by'], $auditIdType);
+		
+		$this->timestamp('updated_at');
+		
+		if (in_array('updated_at', $auditField)) $this->audit(['updated_by'], $auditIdType);
+	}
+	
+	public function softDeletes($auditIdType = false)
+	{
+		$this->timestamp('deleted_at')->nullable();
+		
+		if ($auditType) $this->audit(['deleted_by'], $auditIdType);
+	}
+	
+	public function audit($track = ['created_by', 'updated_by', 'deleted_by'], $auditIdType = 'unsignedInteger')
+	{
+		foreach ($track as $column)
+		{
+			if ($column = 'deleted_by' ? $this->$auditIdType($column, false)->nullable() : $this->$auditIdType($column, false));
+		}
+	}
+}
