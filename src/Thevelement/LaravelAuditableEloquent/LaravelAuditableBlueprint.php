c3cf5adef102d1da91615f3818463d0b17172a29
Vincent Santa Maria <vinny@thevelement.com>
2015-10-04 18:39:52 -0400

    Added custom Blueprint

---
+++ src/Thevelement/LaravelAuditableEloquent/LaravelAuditableBlueprint.php
@@ -0,0 +1,52 @@
+<?php 
+
+namespace Thevelement\LaravelAuditableEloquent;
+
+use Debugbar;
+use Illuminate\Database\Schema\Blueprint;
+
+class LaravelAuditableBlueprint extends Blueprint {
+	/**
+     * Add creation and update timestamps to the table.
+	 * Option to include a "created_by" and "updated_by" fields for auditing purposes.
+     *
+	 * @param array $auditField
+	 * @param string $auditIdType
+     * @return void
+     */
+	public function timestamps($auditField = [], $auditIdType = 'unsignedInteger')
+	{
+		Debugbar::debug('Custom timestamps() called');
+		
+		$this->timestamp('created_at');
+		
+		if (in_array('created_at', $auditField)) $this->audit(['created_by'], $auditIdType);
+		
+		$this->timestamp('updated_at');
+		
+		if (in_array('updated_at', $auditField)) $this->audit(['updated_by'], $auditIdType);
+	}
+	
+	/**
+     * Add a "deleted at" timestamp for the table.
+	 * Option to include a "deleted_by" field for auditing purposes.
+     *
+	 * @param bool|string $auditIdType
+     * @return \Illuminate\Support\Fluent
+     */
+	public function softDeletes($auditIdType = false)
+	{
+		$this->timestamp('deleted_at')->nullable();
+		
+		if ($auditType) $this->audit(['deleted_by'], $auditIdType);
+	}
+	
+	public function audit($track = ['created_by', 'updated_by', 'deleted_by'], $auditIdType = 'unsignedInteger')
+	{
+		foreach ($track as $column)
+		{
+			Debugbar::debug('audit() called for ' . $column);
+			if ($column = 'deleted_by' ? $this->$auditIdType($column, false)->nullable() : $this->$auditIdType($column, false));
+		}
+	}	
+}
--- src/Thevelement/LaravelAuditableEloquent/LaravelAuditableSchemaBuilder.php
+++ src/Thevelement/LaravelAuditableEloquent/LaravelAuditableSchemaBuilder.php
@@ -1,25 +1,8 @@
-<<<<<<< HEAD
-<?php 
-
-namespace Thevelement\LaravelAuditableEloquent;
-=======
 <?php namespace Thevelement\LaravelAuditableEloquent;
->>>>>>> e38d80c49ed2914c43cfe285536594345062eade
 
 use Illuminate\Database\Schema\Builder;
 
 class LaravelAuditableSchemaBuilder extends Builder {
-<<<<<<< HEAD
-	public function timestamps($auditField = [], $auditIdType = 'unsignedInteger')
-	{
-		$this->timestamp('created_at');
-		
-		if (in_array('created_at', $auditField)) $this->audit(['created_by'], $auditIdType);
-		
-		$this->timestamp('updated_at');
-		
-		if (in_array('updated_at', $auditField)) $this->audit(['updated_by'], $auditIdType);
-=======
 	public function timestamps($auditField = array(), $auditIdType = 'unsignedInteger')
 	{
 		$this->timestamp('created_at');
@@ -29,24 +12,16 @@ class LaravelAuditableSchemaBuilder extends Builder {
 		$this->timestamp('updated_at');
 		
 		if (in_array('updated_at', $auditField)) $this->audit(array('updated_by'), $auditIdType);
->>>>>>> e38d80c49ed2914c43cfe285536594345062eade
 	}
 	
 	public function softDeletes($auditIdType = false)
 	{
 		$this->timestamp('deleted_at')->nullable();
 		
-<<<<<<< HEAD
-		if ($auditType) $this->audit(['deleted_by'], $auditIdType);
-	}
-	
-	public function audit($track = ['created_by', 'updated_by', 'deleted_by'], $auditIdType = 'unsignedInteger')
-=======
 		if ($auditType) $this->audit(array('deleted_by'), $auditIdType);
 	}
 	
 	public function audit($track = array('created_by', 'updated_by', 'deleted_by'), $auditIdType = 'unsignedInteger')
->>>>>>> e38d80c49ed2914c43cfe285536594345062eade
 	{
 		foreach ($track as $column)
 		{
